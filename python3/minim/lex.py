1f195b1e06867a5b5718e7ee21928ebfaf37a604
Jonathan Giddy <jongiddy@gmail.com>
2015-04-29 21:56:42 +0100

    Docstrings for get_token and set_token

--- python3/minim/lex.py
+++ python3/minim/lex.py
@@ -26,6 +26,16 @@ def token_type_generator(string_iter):
 
 class token_types:
 
+    """Create an iterator that returns a sequence of token types.
+
+    A token type is a pre-allocated object that can be interrogated to
+    determine the type of the current token in the input stream.  If the
+    token is of interest to the token processor, it can then retrieve
+    an actual token, but this may involve memory allocation operations
+    (to create the token and also to slice the value out of the input
+    stream).
+    """
+
     def __init__(self, string_iter):
         self.generator = token_type_generator(string_iter)
 
@@ -36,6 +46,16 @@ class token_types:
         return next(self.generator)
 
     def get_token(self, token_type):
+        """Return the current token in the input stream.
+
+        If the token type is of interest to the processor, calling this
+        method will allocate and return an actual token, which can be
+        processed further.
+
+        :param token_type: A Token Type returned from the iterator
+        :return: The current token in the input stream
+        :rtype: minim.tokens.Token
+        """
         if token_type.is_token:
             # The iterator is permitted to return a token for next()
             # rather than a token_type.
@@ -44,6 +64,22 @@ class token_types:
             return self.generator.send(token_type)
 
     def set_token(self, token_type, token):
+        """Return the current token in the input stream.
+
+        This function differs from ``get_token`` in that it may choose
+        to use the provided (subclass of) ``Token`` as the returned
+        token.  This allows the opportunity for the system to avoid
+        allocating memory for the token.
+
+        Note that this method is not required to use the provided token,
+        so the return value must be used for processing the current
+        token.
+
+        :param token_type: A Token Type returned from the iterator
+        :param token: A Token that can be set
+        :return: The current token
+        :rtype: minim.tokens.Token
+        """
         if token_type.is_token:
             return token_type
         else:
