5ca80061601499145238def68256e7c5bc51fd7d
Jonathan Giddy <jongiddy@gmail.com>
2015-05-11 08:04:21 +0100

    Make buffer fields private

--- python3/minim/buf.py
+++ python3/minim/buf.py
@@ -3,10 +3,10 @@ class Buffer:
     """The Buffer class makes an iterable look like a single buffer."""
 
     def __init__(self, string_iter):
-        self.iter = iter(string_iter)
-        self.buf = None
-        self.start = 0
-        self.current = 0
+        self._iter = iter(string_iter)
+        self._buf = None
+        self._start = 0
+        self._current = 0
 
     def ensure(self, n=1):
         """Ensure that a minimum number of characters are available.
@@ -18,10 +18,10 @@ class Buffer:
             was reached before reading ``n`` characters
         :raise: EOFError if the buffer is empty and the EOF is reached
         """
-        buf = self.buf
+        buf = self._buf
         if buf is None:
-            buf = self.buf = next(self.iter)
-        current = self.current
+            buf = self._buf = next(self._iter)
+        current = self._current
         if len(buf) - current < n:
             if current > 0:
                 # This may be called before we have reached the end of
@@ -32,7 +32,7 @@ class Buffer:
             # Read more data until at least ``n`` bytes are available.
             while len(buf) < n:
                 try:
-                    buf += next(self.iter)
+                    buf += next(self._iter)
                 except StopIteration:
                     # If we get StopIteration, we may have some stuff left
                     # in buf, which we need to process first.
@@ -40,25 +40,25 @@ class Buffer:
                     # now have nothing left to process, in which case we
                     # propagate the exception.
                     if buf:
-                        self.buf = buf
-                        self.current = current
+                        self._buf = buf
+                        self._current = current
                         return -1
                     else:
                         # We really have reached the end
                         raise EOFError()
-            self.buf = buf
-            self.current = current
-        assert self.buf
+            self._buf = buf
+            self._current = current
+        assert self._buf
         return current
 
     def get(self):
         """Return the character at the current location."""
         current = self.ensure(1)
-        return self.buf[current]
+        return self._buf[current]
 
     def advance(self, n=1):
         """Advance the current location."""
-        self.current += n
+        self._current += n
 
     def next(self):
         """Move to the next character, and return it."""
@@ -66,9 +66,9 @@ class Buffer:
         return self.get()
 
     def extract(self):
-        buf = self.buf
-        start = self.start
-        current = self.current
+        buf = self._buf
+        start = self._start
+        current = self._current
         if start == 0 and current == len(buf):
             return buf
         else:
@@ -79,13 +79,13 @@ class Buffer:
         :return: -1 if no match, else known number of characters after match
         """
         current = self.ensure(1)
-        result = pat.match(self.buf, current)
+        result = pat.match(self._buf, current)
         if result:
-            self.start = current
-            self.current = result.end()
-            return len(self.buf) - self.current
+            self._start = current
+            self._current = result.end()
+            return len(self._buf) - self._current
         else:
-            self.start = current
+            self._start = current
             return -1
 
     def match_to_sentinel(self, sentinel):
@@ -94,14 +94,14 @@ class Buffer:
         :return int: the index at which the sentinel was found, or -1 if
             the sentinel was not found."""
         start = self.ensure(len(sentinel))
-        buf = self.buf
+        buf = self._buf
         if start < 0:
             # EOF and remaining characters < sentinel length
             # -> sentinel can never appear
-            self.start = self.current
-            self.current = len(buf)
+            self._start = self._current
+            self._current = len(buf)
             return -1
-        self.start = start
+        self._start = start
         loc = buf.find(sentinel, start)
         if loc < 0:
             # If sentinel is not found, check whether the last characters
@@ -111,20 +111,20 @@ class Buffer:
                 sentinel = sentinel[:-1]
                 while not buf.endswith(sentinel):
                     sentinel = sentinel[:-1]
-                self.current = len(buf) - len(sentinel)
+                self._current = len(buf) - len(sentinel)
             else:
-                self.current = len(buf)
+                self._current = len(buf)
             return -1
         else:
-            self.current = loc
+            self._current = loc
             return loc - start
 
     def starts_with(self, s):
         """Test whether the current buffer starts with a string."""
         start = self.ensure(len(s))
-        if start < 0 or not self.buf.startswith(s, start):
+        if start < 0 or not self._buf.startswith(s, start):
             return False
         else:
-            self.start = start
-            self.current = start + len(s)
+            self._start = start
+            self._current = start + len(s)
             return True
