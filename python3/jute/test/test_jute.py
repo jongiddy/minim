f9524eebebce28ceda0082c08d1358061b94e71d
Jonathan Giddy <jongiddy@gmail.com>
2015-09-08 08:25:54 +0100

    Refactor interface tests to reduce repetition

--- python3/jute/test/test_jute.py
+++ python3/jute/test/test_jute.py
@@ -5,7 +5,6 @@ from jute import Interface, Dynamic, InterfaceConformanceError
 
 # Simple interface hierarchy for testing
 
-
 class Foo(Interface):
     foo = 99
 
@@ -22,51 +21,16 @@ class FooBaz(Foo):
         """The baz method."""
 
 
-class FooBarBaz(FooBar.Provider):
+class BaseProviderTestsMixin:
 
-    """FooBar provider class.
-
-    A class which implements FooBar, and looks like FooBaz, but does
-    not implement FooBaz.
-    """
-
-    foo = 1
-
-    def bar(self):
-        self.foo = 2
-
-    def baz(self):
-        self.foo = 3
-
-
-class FooBarProxy(Dynamic.Provider):
-
-    def provides_interface(self, interface):
-        if issubclass(FooBar, interface):
-            return True
-        return False
-
-    _foo = 1
-
-    def bar(self):
-        self._foo = 2
-
-    def baz(self):
-        self._foo = 3
-
-    def __getattr__(self, name):
-        if name == 'foo':
-            return self._foo
-        return super().__getattr__(name)
-
-
-class InterfaceProviderTests(unittest.TestCase):
+    # Change these to the classes to be tested
+    HasFooBarBaz = object
+    HasBarOnly = object
 
     def test_provide(self):
         # An interface only has attributes defined in interface (and in
         # sub-interfaces).
-
-        obj = FooBarBaz()
+        obj = self.HasFooBarBaz()
         foobar = FooBar(obj)
         self.assertEqual(foobar.foo, 1)
         foobar.bar()
@@ -76,7 +40,7 @@ class InterfaceProviderTests(unittest.TestCase):
         self.assertEqual(obj.foo, 2)
 
     def test_inherit(self):
-        obj = FooBarBaz()
+        obj = self.HasFooBarBaz()
         foobar = FooBar(obj)
         foo = Foo(foobar)
         self.assertEqual(foo.foo, 1)
@@ -97,13 +61,7 @@ class InterfaceProviderTests(unittest.TestCase):
         an AttributeError, but this may occur later, when it is more
         difficult to diagnose where the invalid object came from.
         """
-        class Bar(FooBar.Provider):
-            # doesn't implement foo
-
-            def bar():
-                pass
-
-        obj = Bar()
+        obj = self.HasBarOnly()
         if __debug__:
             with self.assertRaises(InterfaceConformanceError):
                 FooBar(obj)
@@ -114,31 +72,19 @@ class InterfaceProviderTests(unittest.TestCase):
 
     def test_incomplete_implementation_validate_true(self):
         """validate is True -> always raise InterfaceConformanceError."""
-        class Bar(FooBar.Provider):
-            # doesn't implement foo
-
-            def bar():
-                pass
-
-        obj = Bar()
+        obj = self.HasBarOnly()
         with self.assertRaises(InterfaceConformanceError):
             FooBar(obj, validate=True)
 
     def test_incomplete_implementation_validate_false(self):
         """validate is False -> always raise late AttributeError."""
-        class Bar(FooBar.Provider):
-            # doesn't implement foo
-
-            def bar():
-                pass
-
-        obj = Bar()
+        obj = self.HasBarOnly()
         foobar = FooBar(obj, validate=False)
         with self.assertRaises(AttributeError):
             foobar.foo
 
     def test_upcast_fails(self):
-        obj = FooBarBaz()
+        obj = self.HasFooBarBaz()
         foo = Foo(obj)
         with self.assertRaises(TypeError):
             FooBar(foo)
@@ -149,7 +95,7 @@ class InterfaceProviderTests(unittest.TestCase):
         Although object matches the interface by duck-typing, it does
         not claim to provide the interface, so it fails with a
         TypeError."""
-        obj = FooBarBaz()
+        obj = self.HasFooBarBaz()
         with self.assertRaises(TypeError):
             FooBaz(obj)
 
@@ -159,15 +105,15 @@ class InterfaceProviderTests(unittest.TestCase):
         Although object matches the interface by duck-typing, it does
         not claim to provide the interface, so it fails with a
         TypeError."""
-        obj = FooBarBaz()
+        obj = self.HasFooBarBaz()
         foobar = FooBar(obj)
         with self.assertRaises(TypeError):
             FooBaz(foobar)
 
     def test_subclass_provider_provides_interface(self):
-        class FooBarBazSub(FooBarBaz, FooBaz.Provider):
+        class FooBarBazSubclass(self.HasFooBarBaz, FooBaz.Provider):
             pass
-        obj = FooBarBazSub()
+        obj = FooBarBazSubclass()
         foobar = FooBar(obj)
         foobaz = FooBaz(obj)
         self.assertEqual(foobar.foo, 1)
@@ -177,9 +123,9 @@ class InterfaceProviderTests(unittest.TestCase):
         self.assertEqual(obj.foo, 3)
 
     def test_provider_subclass_provides_interface(self):
-        class FooBarBazSub(FooBaz.Provider, FooBarBaz):
+        class FooBarBazSubclass(FooBaz.Provider, self.HasFooBarBaz):
             pass
-        obj = FooBarBazSub()
+        obj = FooBarBazSubclass()
         foobar = FooBar(obj)
         foobaz = FooBaz(obj)
         self.assertEqual(foobar.foo, 1)
@@ -189,81 +135,76 @@ class InterfaceProviderTests(unittest.TestCase):
         self.assertEqual(obj.foo, 3)
 
 
-class DynamicProviderTests(unittest.TestCase):
+class FooBarBaz(FooBar.Provider):
 
-    def test_provide(self):
-        # An interface only has attributes defined in interface (and in
-        # sub-interfaces).
+    """FooBar provider class.
 
-        obj = FooBarProxy()
-        foobar = FooBar(obj)
-        self.assertEqual(foobar.foo, 1)
-        foobar.bar()
-        self.assertEqual(obj.foo, 2)
-        with self.assertRaises(AttributeError):
-            foobar.baz()
-        self.assertEqual(obj.foo, 2)
+    A class which implements FooBar, and looks like FooBaz, but does
+    not implement FooBaz.
+    """
 
-    def test_inherit(self):
-        obj = FooBarProxy()
-        foobar = FooBar(obj)
-        foo = Foo(foobar)
-        self.assertEqual(foo.foo, 1)
-        with self.assertRaises(AttributeError):
-            foo.bar()
-        self.assertEqual(obj.foo, 1)
+    foo = 1
 
-    def test_incomplete_implementation_validate_none(self):
-        """Incomplete implementations are caught (during debugging)."""
-        class BarProxy(Dynamic.Provider):
-            # doesn't implement foo
+    def bar(self):
+        self.foo = 2
 
-            def provides_interface(self, interface):
-                if issubclass(FooBar, interface):
-                    return True
-                return False
+    def baz(self):
+        self.foo = 3
 
-            def bar():
-                pass
 
-        obj = BarProxy()
-        if __debug__:
-            with self.assertRaises(InterfaceConformanceError):
-                FooBar(obj)
-        else:
-            foobar = FooBar(obj)
-            with self.assertRaises(AttributeError):
-                foobar.foo
+class IncompleteFooBar(FooBar.Provider):
+    # doesn't implement foo
 
-    def test_upcast_fails(self):
-        obj = FooBarProxy()
-        foo = Foo(obj)
-        with self.assertRaises(TypeError):
-            FooBar(foo)
+    def bar():
+        pass
 
-    def test_duck_fails(self):
-        """Duck-typed object does not match unclaimed interface.
 
-        Although object matches the interface by duck-typing, it does
-        not claim to provide the interface, so it fails with a
-        TypeError."""
-        obj = FooBarProxy()
-        with self.assertRaises(TypeError):
-            FooBaz(obj)
+class InterfaceProviderTests(BaseProviderTestsMixin, unittest.TestCase):
 
-    def test_wrapped_duck_fails(self):
-        """Duck-typed wrapped object does not match unclaimed interface.
+    HasFooBarBaz = FooBarBaz
+    HasBarOnly = IncompleteFooBar
 
-        Although object matches the interface by duck-typing, it does
-        not claim to provide the interface, so it fails with a
-        TypeError."""
-        obj = FooBarProxy()
-        foobar = FooBar(obj)
-        with self.assertRaises(TypeError):
-            FooBaz(foobar)
+
+class FooBarBazDynamic(Dynamic.Provider):
+
+    def provides_interface(self, interface):
+        if issubclass(FooBar, interface):
+            return True
+        return False
+
+    _foo = 1
+
+    def bar(self):
+        self._foo = 2
+
+    def baz(self):
+        self._foo = 3
+
+    def __getattr__(self, name):
+        if name == 'foo':
+            return self._foo
+        return super().__getattr__(name)
+
+
+class IncompleteFooBarDynamic(Dynamic.Provider):
+    # doesn't implement foo
+
+    def provides_interface(self, interface):
+        if issubclass(FooBar, interface):
+            return True
+        return False
+
+    def bar():
+        pass
+
+
+class DynamicProviderTests(BaseProviderTestsMixin, unittest.TestCase):
+
+    HasFooBarBaz = FooBarBazDynamic
+    HasBarOnly = IncompleteFooBarDynamic
 
     def test_subclass_provides_interface(self):
-        class FooBarBazSub(FooBarProxy):
+        class FooBarBazSubclass(self.HasFooBarBaz):
 
             def provides_interface(self, interface):
                 if (
@@ -273,7 +214,7 @@ class DynamicProviderTests(unittest.TestCase):
                     return True
                 return False
 
-        obj = FooBarBazSub()
+        obj = FooBarBazSubclass()
         foobar = FooBar(obj)
         foobaz = FooBaz(obj)
         self.assertEqual(foobar.foo, 1)
@@ -292,47 +233,58 @@ class Iterable(Interface):
         """Interface for an iterable."""
 
 
+class IterTestMixin:
+
+    def get_test_object(self):
+        return object()
+
+    def test_for(self):
+        cnt = self.get_test_object()
+        expected = list(count_to_5)
+        result = []
+        cnt = self.get_test_object()
+        for val in cnt:
+            result.append(val)
+        self.assertEqual(result, expected)
+
+    def test_iter(self):
+        cnt = self.get_test_object()
+        iterator = iter(cnt)
+        self.assertEqual(next(iterator), 0)
+
+    def test_getattr(self):
+        cnt = self.get_test_object()
+        iterator = getattr(cnt, '__iter__')()
+        self.assertEqual(next(iterator), 0)
+
+    def test_attribute(self):
+        cnt = self.get_test_object()
+        iterator = cnt.__iter__()
+        self.assertEqual(next(iterator), 0)
+
+
 class CountTo5(Iterable.Provider):
 
     def __iter__(self):
         return iter(count_to_5)
 
 
-class SpecialMethodTests(unittest.TestCase):
+class IterInstanceTests(IterTestMixin, unittest.TestCase):
 
-    def test_for_instance(self):
-        expected = list(count_to_5)
-        result = []
-        cnt = CountTo5()
-        for val in cnt:
-            result.append(val)
-        self.assertEqual(result, expected)
+    def get_test_object(self):
+        return CountTo5()
 
-    def test_for_interface(self):
-        expected = list(count_to_5)
-        result = []
-        cnt = Iterable(CountTo5())
-        for val in cnt:
-            result.append(val)
-        self.assertEqual(result, expected)
 
-    def test_getattr(self):
-        cnt = Iterable(CountTo5())
-        iterator = getattr(cnt, '__iter__')()
-        self.assertEqual(next(iterator), 0)
+class IterInterfaceTests(IterTestMixin, unittest.TestCase):
 
-    def test_iter(self):
-        cnt = Iterable(CountTo5())
-        iterator = iter(cnt)
-        self.assertEqual(next(iterator), 0)
-
-    def test_attribute(self):
-        cnt = Iterable(CountTo5())
-        iterator = cnt.__iter__()
-        self.assertEqual(next(iterator), 0)
+    def get_test_object(self):
+        return Iterable(CountTo5())
 
 
 class IterableProxy(Dynamic.Provider):
+
+    def __init__(self, wrapped_iterable):
+        self.wrapped = wrapped_iterable
 
     def provides_interface(self, interface):
         if issubclass(Iterable, interface):
@@ -340,44 +292,18 @@ class IterableProxy(Dynamic.Provider):
         return False
 
     def __iter__(self):
-        return iter(count_to_5)
+        return iter(self.wrapped)
 
 
-class DynamicSpecialMethodTests(unittest.TestCase):
+class IterDynamicInterfaceTests(IterTestMixin, unittest.TestCase):
 
-    def test_for_instance(self):
-        expected = list(count_to_5)
-        result = []
-        cnt = IterableProxy()
-        for val in cnt:
-            result.append(val)
-        self.assertEqual(result, expected)
-
-    def test_for_interface(self):
-        expected = list(count_to_5)
-        result = []
-        cnt = Iterable(IterableProxy())
-        for val in cnt:
-            result.append(val)
-        self.assertEqual(result, expected)
-
-    def test_getattr(self):
-        cnt = Iterable(IterableProxy())
-        iterator = getattr(cnt, '__iter__')()
-        self.assertEqual(next(iterator), 0)
-
-    def test_iter(self):
-        cnt = Iterable(IterableProxy())
-        iterator = iter(cnt)
-        self.assertEqual(next(iterator), 0)
-
-    def test_attribute(self):
-        cnt = Iterable(IterableProxy())
-        iterator = cnt.__iter__()
-        self.assertEqual(next(iterator), 0)
+    def get_test_object(self):
+        return IterableProxy(CountTo5())
 
 
 class GeneratedIterCount5(Iterable.Provider):
+
+    """A class that generates the __iter__ dynamically."""
 
     def __getattr__(self, name):
         if name == '__iter__':
@@ -386,41 +312,36 @@ class GeneratedIterCount5(Iterable.Provider):
             return f
 
 
-class IterSpecialMethodTests(unittest.TestCase):
+class GeneratedIterTestMixin(IterTestMixin):
 
     """Test __iter__ for a provider that generates __iter__.
 
-    Using a dynamicaly generated __iter__ method fails when using `for`
-    on an object.  To minimise surprise, the interface should behave the
-    same way.
+    Using a dynamicaly generated __iter__ method fails when using `iter`
+    or `for` on an object.  To minimise surprise, the interface should
+    behave the same way as a normal instance.
+
+    Note, that getting the attribute succeeds otherwise.
     """
 
-    def test_iter_for_instance(self):
-        cnt = GeneratedIterCount5()
+    def test_for(self):
+        cnt = self.get_test_object()
+        with self.assertRaises(TypeError):
+            for val in cnt:
+                pass
+
+    def test_iter(self):
+        cnt = self.get_test_object()
         with self.assertRaises(TypeError):
             iter(cnt)
 
-    def test_iter_for_interface(self):
-        cnt = Iterable(GeneratedIterCount5())
-        with self.assertRaises(TypeError):
-            iter(cnt)
 
-    def test_getattr_for_instance(self):
-        cnt = GeneratedIterCount5()
-        iterator = getattr(cnt, '__iter__')()
-        self.assertEqual(next(iterator), 0)
+class GeneratedIterInstanceTests(GeneratedIterTestMixin, unittest.TestCase):
 
-    def test_getattr_for_interface(self):
-        cnt = Iterable(GeneratedIterCount5())
-        iterator = getattr(cnt, '__iter__')()
-        self.assertEqual(next(iterator), 0)
+    def get_test_object(self):
+        return GeneratedIterCount5()
 
-    def test_attribute_for_instance(self):
-        cnt = GeneratedIterCount5()
-        iterator = cnt.__iter__()
-        self.assertEqual(next(iterator), 0)
 
-    def test_attribute_for_interface(self):
-        cnt = Iterable(GeneratedIterCount5())
-        iterator = cnt.__iter__()
-        self.assertEqual(next(iterator), 0)
+class GeneratedIterInterfaceTests(GeneratedIterTestMixin, unittest.TestCase):
+
+    def get_test_object(self):
+        return Iterable(GeneratedIterCount5())
